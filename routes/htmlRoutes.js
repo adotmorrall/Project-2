8e7c9206b386bdc0533f7b34830f705167240ce9
Andre' Morrall <adotmorrall@gmail.com>
2020-01-12 12:52:56 -0600

    Add files via upload
    Add route to dashboard.html

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,27 +1,50 @@
 var db = require("../models");
+var path = require('path');
 
-module.exports = function(app) {
+module.exports = function (app) {
   // Load index page
-  app.get("/", function(req, res) {
-    db.Example.findAll({}).then(function(dbExamples) {
-      res.render("index", {
+  app.get("/", function (req, res) {
+    res.render("index");
+  });
+
+  //Load login page
+  app.get("/login", function (req, res) {
+    res.render("login");
+  });
+
+  //Load register page
+  app.get("/register", function (req, res) {
+    res.render("register");
+  });
+
+  //Load dashboard page
+  app.get("/dashboard", function (req, res) {
+    res.sendFile(path.join(__dirname, "/../views/dashboard.html"));
+  });
+
+  // Load reviews page
+  app.get("/reviews", function (req, res) {
+    db.Review.findAll({}).then(function (dbReviews) {
+      res.render("reviews", {
         msg: "Welcome!",
-        examples: dbExamples
+        reviews: dbReviews
       });
     });
   });
 
-  // Load example page and pass in an example by id
-  app.get("/example/:id", function(req, res) {
-    db.Example.findOne({ where: { id: req.params.id } }).then(function(dbExample) {
-      res.render("example", {
-        example: dbExample
+  // Load review page and pass in an review by id
+  app.get("/review/:id", function (req, res) {
+    db.Review.findOne({ where: { id: req.params.id } }).then(function (
+      dbReview
+    ) {
+      res.render("review", {
+        review: dbReview
       });
     });
   });
 
   // Render 404 page for any unmatched routes
-  app.get("*", function(req, res) {
+  app.get("*", function (req, res) {
     res.render("404");
   });
 };
