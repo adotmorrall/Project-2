777a52c08d9fb087222a03444ba7a8ac07a00798
Andre' Morrall <adotmorrall@gmail.com>
2020-01-12 12:51:31 -0600

    Add files via upload
    Add route to dashboard.html

---
+++ htmlRoutes.js
@@ -0,0 +1,50 @@
+var db = require("../models");
+var path = require('path');
+
+module.exports = function (app) {
+  // Load index page
+  app.get("/", function (req, res) {
+    res.render("index");
+  });
+
+  //Load login page
+  app.get("/login", function (req, res) {
+    res.render("login");
+  });
+
+  //Load register page
+  app.get("/register", function (req, res) {
+    res.render("register");
+  });
+
+  //Load dashboard page
+  app.get("/dashboard", function (req, res) {
+    res.sendFile(path.join(__dirname, "/../views/dashboard.html"));
+  });
+
+  // Load reviews page
+  app.get("/reviews", function (req, res) {
+    db.Review.findAll({}).then(function (dbReviews) {
+      res.render("reviews", {
+        msg: "Welcome!",
+        reviews: dbReviews
+      });
+    });
+  });
+
+  // Load review page and pass in an review by id
+  app.get("/review/:id", function (req, res) {
+    db.Review.findOne({ where: { id: req.params.id } }).then(function (
+      dbReview
+    ) {
+      res.render("review", {
+        review: dbReview
+      });
+    });
+  });
+
+  // Render 404 page for any unmatched routes
+  app.get("*", function (req, res) {
+    res.render("404");
+  });
+};
